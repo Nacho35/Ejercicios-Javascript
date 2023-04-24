7ec47489cc352a09ffbb266a619e1e5fe6cb2f6b
unknown <ignacioevil@gmail.com>
2023-04-23 21:47:30 -0300

    practicando js

---
+++ index.js
@@ -0,0 +1,238 @@
+//* En este ejercicio, se te pide que calcules y imprimas la suma de los elementos de un arreglo, teniendo en cuenta que algunos de estos números pueden ser bastante grandes.
+
+//* La función que debes completar se llama "aVeryBigSum" y tiene un parámetro: un arreglo de enteros llamado "ar". La función debe devolver la suma de todos los elementos del arreglo.
+
+//* La entrada se proporciona en el siguiente formato: la primera línea contiene un entero "n", que indica la cantidad de elementos en el arreglo. La segunda línea contiene "n" enteros separados por espacios, que son los elementos del arreglo.
+
+//* La salida debe ser un número entero que representa la suma de los elementos del arreglo.
+
+//* Para resolver este ejercicio, puedes recorrer el arreglo "ar" utilizando un bucle for y acumular la suma de los elementos en una variable. Dado que algunos de los enteros pueden ser muy grandes, es importante tener en cuenta que el tipo de datos de la variable acumuladora puede tener que ser un "long" para evitar desbordamientos.
+
+function aVeryBigSum(ar) {
+	let sum = 0;
+	for (let i = 0; i < ar.length; i++) {
+		sum += ar[i];
+	}
+	return sum;
+}
+
+//* Este ejercicio se llama "Diagonal Difference" y se trata de encontrar la diferencia absoluta entre la suma de los elementos de la diagonal principal y la suma de los elementos de la diagonal secundaria de una matriz cuadrada.
+
+//* El enunciado pide que implementes una función llamada "diagonalDifference" que reciba como argumento una matriz cuadrada (un arreglo de arreglos) y devuelva la diferencia absoluta entre las sumas de los elementos de las diagonales principal y secundaria.
+
+//* Para resolver este ejercicio, puedes recorrer la matriz utilizando dos bucles for anidados y sumar los elementos de ambas diagonales. Luego, calcular la diferencia absoluta entre ambas sumas.
+
+function diagonalDifference(arr) {
+	let n = arr.length;
+	let diagonal1 = 0;
+	let diagonal2 = 0;
+	for (let i = 0; i < n; i++) {
+		diagonal1 += arr[i][i];
+		diagonal2 += arr[i][n - i - 1];
+	}
+	return Math.abs(diagonal1 - diagonal2);
+}
+
+//* consiste en calcular la fracción de números positivos, negativos y ceros en una lista de números enteros.
+
+//* Para resolver este problema, primero debes leer la lista de números enteros y almacenarla en un array.
+
+//* Luego, debes declarar tres variables para contar la cantidad de números positivos, negativos y ceros. Inicializa las variables en cero.
+
+//* A continuación, puedes utilizar un bucle for para recorrer el array y verificar si cada número es positivo, negativo o cero. Puedes utilizar la estructura if-else para esto.
+
+//* Si el número es positivo, incrementa la variable que cuenta los números positivos en uno. Si es negativo, incrementa la variable que cuenta los números negativos en uno. Si es cero, incrementa la variable que cuenta los ceros en uno.
+
+//* Después de recorrer todo el array, calcula la fracción de cada tipo de número. Puedes hacer esto dividiendo la cantidad de números positivos, negativos o ceros por el total de números en el array.
+
+//* Finalmente, muestra los resultados utilizando console.log(). Recuerda que debes mostrar los resultados con 6 decimales de precisión.
+
+function plusMinus(arr) {
+	let positiveCount = 0;
+	let negativesCount = 0;
+	let zerosCount = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > 0) {
+			positiveCount++;
+		} else if (arr[i] < 0) {
+			negativesCount++;
+		} else {
+			zerosCount++;
+		}
+	}
+	const positiveFraction = positiveCount / arr.length;
+	const negativeFraction = negativesCount / arr.length;
+	const zerosFraction = zerosCount / arr.length;
+
+	console.log(positiveFraction.toFixed(6));
+	console.log(negativeFraction.toFixed(6));
+	console.log(zerosFraction.toFixed(6));
+}
+
+//* Escribir una función que imprima una escalera de caracteres "#" con una altura específica "n". La escalera debe estar alineada a la derecha. Por ejemplo, si n = 6, la salida debería verse así:
+//*   #
+//*   ##
+//*  	###
+//* 	####
+//* 	#####
+//* 	######
+
+//* Para resolver este problema, necesitarás utilizar una estructura de bucle anidado. El bucle exterior se encargará de imprimir cada fila de la escalera, y el bucle interior se encargará de imprimir los caracteres "#" y los espacios en blanco.
+
+//* Una forma de enfocar este problema es imprimir primero los espacios en blanco necesarios para alinear la escalera a la derecha, y luego imprimir los caracteres "#" necesarios para crear la escalera. Puedes utilizar la función repeat() de JavaScript para imprimir un carácter o cadena de caracteres un número determinado de veces.
+
+//* Asegúrate de que la escalera tenga el mismo número de caracteres "#" en cada fila y que las filas estén alineadas a la derecha.
+
+function staircase(n) {
+	let stairs = new Array(n).fill(" ");
+
+	stairs.forEach((stair, index) => {
+		stairs[n - index - 1] = "#";
+		console.log(stairs.join(""));
+	});
+}
+staircase(6);
+
+//* Dado un arreglo de enteros, escribir una función que revierta el orden de los elementos del arreglo.
+
+//* Por ejemplo, si el arreglo es [1, 2, 3, 4, 5], la función debería retornar [5, 4, 3, 2, 1].
+
+//* Donde a es el arreglo de entrada.
+
+//* Para resolver el problema, aquí hay algunos pasos que puedes seguir:
+
+//* Crea un nuevo arreglo vacío.
+//* Recorre el arreglo de entrada desde el último elemento hasta el primero.
+//* En cada iteración, agrega el elemento actual al final del nuevo arreglo.
+//* Retorna el nuevo arreglo.
+
+function reverseArray(a) {
+	let arr = [];
+	for (let i = a.length - 1; i >= 0; i--) {
+		arr.push(a[i]);
+	}
+	return arr;
+}
+
+//* te pide que encuentres cuántas velas en un pastel son las más altas. El pastel se representa como un array de enteros donde cada elemento del array representa la altura de una vela.
+
+//* Para resolver este problema, necesitamos encontrar la altura máxima de las velas y luego contar cuántas velas tienen esa altura.
+
+//* El proceso para resolver el problema sería el siguiente:
+
+//* Inicializar dos variables: maxHeight y count a 0. Estas variables se utilizarán para rastrear la altura máxima de las velas y el número de velas que tienen esa altura, respectivamente.
+
+//* Iterar sobre el array de velas y para cada vela:
+
+//* a. Si la altura de la vela es mayor que maxHeight, actualiza maxHeight a la altura de la vela y restablece count a 1.
+
+//* b. Si la altura de la vela es igual a maxHeight, aumenta count en 1.
+
+//* Devuelve el valor de count.
+
+function birthdayCakeCandles(candles) {
+	let maxHeight = 0;
+	let count = 0;
+
+	for (let i = 0; i < candles.length; i++) {
+		if (candles[i] > maxHeight) {
+			maxHeight = candles[i];
+			count = 1;
+		} else if (candles[i] === maxHeight) {
+			count++;
+		}
+	}
+	return count;
+}
+
+//* El problema "Mini-Max Sum" pide que le des cinco números enteros diferentes como entrada y, a continuación, debes calcular la suma mínima y la suma máxima de cuatro de los cinco números. En otras palabras, debes sumar cuatro de los cinco números y encontrar tanto la suma mínima como la suma máxima posible.
+
+//* Por ejemplo, si los cinco números que se te dan como entrada son [1, 2, 3, 4, 5], entonces la suma mínima posible es 1 + 2 + 3 + 4 = 10 y la suma máxima posible es 2 + 3 + 4 + 5 = 14.
+
+//* Una vez que hayas encontrado ambas sumas, debes imprimir en pantalla ambas sumas en una sola línea, separadas por un espacio.
+
+function miniMaxSum(arr) {
+	arr.sort((a, b) => a - b);
+	const minSum = arr.slice(0, 4).reduce((acc, curr) => acc + curr);
+	arr.sort((a, b) => b - a);
+	const maxSum = arr.slice(0, 4).reduce((acc, curr) => acc + curr);
+	console.log(minSum + " " + maxSum);
+}
+
+//* pide que conviertas una hora en formato de 12 horas (AM/PM) en un formato de 24 horas.
+
+//* El input es una cadena de texto en el formato hh:mm:ssAM o hh:mm:ssPM, donde hh representa la hora en formato de 12 horas, mm representa los minutos y ss representa los segundos. La cadena termina con las letras AM o PM, que indican si la hora es antes o después del mediodía, respectivamente.
+
+//* Para resolver este problema, necesitamos extraer las partes relevantes de la cadena de entrada y convertir la hora a un formato de 24 horas. Para hacerlo, podemos seguir los siguientes pasos:
+
+//* Extraer la hora, los minutos y los segundos de la cadena de entrada.
+
+//* Comprobar si la hora es AM o PM. Si es AM y la hora es 12, la hora debe establecerse en 00. Si es PM y la hora no es 12, agregue 12 a la hora.
+
+//* Devuelve la hora en formato de 24 horas, como una cadena de texto en el formato hh:mm:ss.
+
+function timeConversion(s) {
+	const time = s.split(":");
+	if (s.end("PM") && hour !== 12) {
+		hour += 12;
+	} else if (s.end("AM") && hour === 12) {
+		hour = 0;
+	}
+	return `${hour.toString().padStart(2, "0")}:${minutes
+		.toString()
+		.padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+	//! SIN TERMINAR
+}
+
+//* Tengo un gato y un perro.
+
+//* Los conseguí al mismo tiempo que gatito/cachorro. Eso fue humanYearshace años.
+
+//* Devuelve sus respectivas edades ahora como [ humanYears, catYears, dogYears]
+
+//* NOTAS:
+
+//* añoshumanos >= 1
+//* humanYears son solo números enteros
+//* Años del gato
+//* 15años de gato para el primer año
+//* +9años de gato por segundo año
+//* +4cat años por cada año después de eso
+//* Años del perro
+//* 15años de perro para el primer año
+//* +9años de perro por segundo año
+//* +5años de perro por cada año después de eso
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+	let catYears = 0;
+	let dogYears = 0;
+
+	if (humanYears === 1) {
+		catYears = 15;
+		dogYears = 15;
+	} else if (humanYears === 2) {
+		catYears = 24;
+		dogYears = 24;
+	} else if (humanYears > 2) {
+		catYears = 24 + (humanYears - 2) * 4;
+		dogYears = 24 + (humanYears - 2) * 5;
+	}
+	return [humanYears, catYears, dogYears];
+};
+
+//* Completa el método que toma un valor booleano y devuelve una "Yes" cadena para true o una "No" cadena para false.
+
+function boolToWord(bool) {
+	const str = bool === true ? "Yes" : "No";
+	return str;
+}
+
+//* Le preguntas a una niña pequeña: "¿Cuántos años tienes?" Ella siempre dice "x años", donde x es un número aleatorio entre 0 y 9.
+
+//* Escriba un programa que devuelva la edad de la niña (0-9) como un número entero.
+
+//* Suponga que la cadena de entrada de prueba siempre es una cadena válida. Por ejemplo, la entrada de prueba puede ser "1 año" o "5 años". El primer carácter de la cadena siempre es un número.
+
+function getAge(inputString) {
+	// return the girl's correct age as an integer. Happy coding :)
+}
